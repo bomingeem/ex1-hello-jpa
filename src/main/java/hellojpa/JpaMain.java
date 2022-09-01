39d825fbe2b9edcd30a8db55943eda4d9d9ca7ca
bomingeem <bomingeem@gmail.com>
2022-09-01 15:58:39 +0900

    02. JPA 시작하기 복습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,6 +7,11 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
+        /**
+         * - EntityManagerFactory 는 하나만 생성해서 애플리케이션 전체에서 공유
+         * - EntityManager 는 쓰레드간에 공유 X (사용하고 버려야 한다)
+         * - JPA 의 모든 데이터 변경은 트랜잭션 안에서 실행
+         */
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
