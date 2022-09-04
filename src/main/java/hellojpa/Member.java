d515f5f01bf260915e8eafc64c178bcddcbf9e3c
bomingeem <bomingeem@gmail.com>
2022-09-04 10:40:58 +0900

    기본 키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,21 +1,36 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+//@TableGenerator(name = "member_seq_generator", table = "my_sequences", pkColumnValue = "member_seq", allocationSize = 1)
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq", initialValue = 1, allocationSize = 50)
 public class Member {
-
     @Id
+    //@GeneratedValue(strategy = GenerationType.TABLE, generator = "member_seq_generator")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    /**
+     private Integer age;
+     @Enumerated(EnumType.STRING)
+     private RoleType roleType;
+     @Temporal(TemporalType.TIMESTAMP)
+     private Date createdDate;
+     @Temporal(TemporalType.TIMESTAMP)
+     private Date lastModifiedDate;
+     private LocalDate testLocalDate;
+     private LocalDateTime testLocalDateTime;
+     @Lob
+     private String description;
+     @Transient
+     private int temp;
+     */
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -26,11 +41,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
