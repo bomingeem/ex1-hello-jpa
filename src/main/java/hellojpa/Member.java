a2e4551668ff49aea080a198b55aba241166a536
bomingeem <bomingeem@gmail.com>
2022-09-19 22:43:55 +0900

    임베디드 타입

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,13 +3,27 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
     @Id
     @GeneratedValue
     private Long id;
 
     @Column(name = "USERNAME")
     private String username;
+
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     /**
      * @Column(name = "TEAM_ID")
@@ -43,7 +57,23 @@ public class Member extends BaseEntity {
         this.team = team;
     }
 
-//    public void changeTeam(Team team) {
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    //    public void changeTeam(Team team) {
 //        this.team = team;
 //        team.getMembers().add(this);
 //    }
